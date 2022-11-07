5e2e594c7d31432d87cb27f487d4c5400bb57129
Fabio <117052477+fabio-code-1@users.noreply.github.com>
2022-11-07 14:48:49 -0300

    Atualização no banco de dados
    Foi redirecionado os arquivos.txt para uma pasta dataBase

---
+++ connect_pim/libconect.h
@@ -0,0 +1,14 @@
+#ifndef LIBCONECT_H_INCLUDED
+#define LIBCONECT_H_INCLUDED
+
+void Menu_Principal();
+void Menu_Cadastros();
+void Cadastro_Funcionario();
+void Cadastro_Cliente();
+void Relatorio();
+void Menu_Consultar();
+void Menu_Colsutar_Cadastros();
+void Consultar_Relatorio();
+void User_Autorizado();
+void Data_Hora();
+#endif // LIBCONECT_H_INCLUDED
